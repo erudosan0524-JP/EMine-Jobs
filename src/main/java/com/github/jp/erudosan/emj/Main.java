f3d5d78ccfa40c748adcc531c3f54e2bbb9f6dba
erudo <hashierudo@gmail.com>
2020-10-11 16:23:26 +0900

    [new] new files for init
    マイクラプラグインの基礎部分の追加

---
+++ src/main/java/com/github/jp/erudosan/emj/Main.java
@@ -0,0 +1,16 @@
+package com.github.jp.erudosan.emj;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
