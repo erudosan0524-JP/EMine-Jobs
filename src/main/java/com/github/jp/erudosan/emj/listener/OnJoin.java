45d47ef83666d5d441f101e75183d72b43b6c1dc
erudo <hashierudo@gmail.com>
2020-10-18 17:34:56 +0900

    [add] add PlayerJoinEvent
    PlayerJoinEventの処理追加

--- src/main/java/com/github/jp/erudosan/emj/listener/OnJoin.java
+++ src/main/java/com/github/jp/erudosan/emj/listener/OnJoin.java
@@ -1,4 +1,21 @@
 package com.github.jp.erudosan.emj.listener;
 
-public class OnJoin {
+import com.github.jp.erudosan.emj.Main;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class OnJoin implements Listener {
+
+    private Main plugin;
+
+    public OnJoin(Main plugin) {
+        this.plugin = plugin;
+        plugin.getServer().getPluginManager().registerEvents(this,plugin);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+
+    }
 }
