2910b9e4e1ff03414b485a24c1277ea1b61aba26
erudosan0524-JP <Eruvis4869>
2020-10-26 16:04:45 +0900

    [update] update OnJobJoin Listener
    OnJobJoinLeave.javaのアップデート

--- src/main/java/com/github/jp/erudosan/emj/listener/mylistener/OnJobJoinLeave.java
+++ src/main/java/com/github/jp/erudosan/emj/listener/mylistener/OnJobJoinLeave.java
@@ -4,17 +4,20 @@ import com.github.jp.erudosan.emj.Main;
 import com.github.jp.erudosan.emj.event.PlayerJobJoinEvent;
 import com.github.jp.erudosan.emj.event.PlayerJobLeaveEvent;
 import com.github.jp.erudosan.emj.job.Job;
+import com.github.jp.erudosan.emj.job.jobs.fisher.FishPro;
 import com.github.jp.erudosan.emj.job.jobs.miner.Gunner;
 import me.zombie_striker.qg.api.QualityArmory;
 import me.zombie_striker.qg.guns.Gun;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Random;
+import java.util.*;
 
 public class OnJobJoinLeave implements Listener {
 
@@ -34,8 +37,25 @@ public class OnJobJoinLeave implements Listener {
             Gun gun = QualityArmory.getGunByName("ak47");
 
             player.getInventory().addItem(gun.getItemStack());
+            player.getInventory().addItem(gun.getAmmoType().getItemStack());
 
-            player.sendMessage("報酬として銃を手に入れた！");
+            player.sendMessage("報酬として" + ChatColor.GOLD + "銃" +ChatColor.WHITE + "を手に入れた！");
+        } else if(job instanceof FishPro) {
+            ItemStack fishRod = new ItemStack(Material.FISHING_ROD,1);
+            ItemMeta meta = fishRod.getItemMeta();
+
+            String itemName = ChatColor.GOLD + "すごいつりざお";
+            meta.setDisplayName(itemName);
+            List<String> lores = new ArrayList<>();
+            lores.add(ChatColor.DARK_PURPLE + "めちゃくちゃすごいつりざお");
+            meta.setLore(lores);
+            meta.addEnchant(Enchantment.LURE,5,true);
+            meta.addEnchant(Enchantment.DURABILITY,5,true);
+            meta.addEnchant(Enchantment.LUCK,5,true);
+
+            fishRod.setItemMeta(meta);
+
+            player.sendMessage("報酬として" + itemName +ChatColor.WHITE + "を手に入れた！");
         }
     }
 
